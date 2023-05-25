98d260afa60c383ae0467342755097752c936c07
Eid1999 <pedrohenrique_eid@hotmail.com>
2023-05-25 09:25:59 +0100

    fixes

--- main.py
+++ main.py
@@ -62,29 +62,42 @@ class BData():
         reduced = pipeline.inverse_transform(
             pipeline.transform(self.VGG_df))
 
-        self.VGG_df = pd.DataFrame(reduced[:, 0:2])
+        self.VGG_pca = pd.DataFrame(reduced)
 
     def PCA_skelly(self):
-
-        skelly = 0
         steps = [('scaling', StandardScaler()),
                  ('pca', PCA(n_components=0.85))]
         pipeline = Pipeline(steps)
         pipeline.fit(self.skeleton_df)
 
-        self.skeleton_df = pipeline.inverse_transform(
+        self.skeleton_pca = pipeline.inverse_transform(
             pipeline.transform(self.skeleton_df))
 
-    def kmeans(self, df):
-        km = KMeans(n_clusters=2, n_init="auto").fit(df)
+    def kmeans(self, df, k=10):
+        self.cluster_number(df)
+        km = KMeans(n_clusters=k, n_init="auto").fit(df)
         clusters = km.cluster_centers_
         plt.figure(num=None, figsize=(10, 10), dpi=100,
                    facecolor='w', edgecolor='k')
-        plt.scatter(df[:][0], df[:][1], alpha=0.25, s=100, color='red')
-        plt.scatter(clusters[:, 0], clusters[:, 1], s=100000, alpha=0.30)
+        # plt.scatter(df[:][0], df[:][1], alpha=0.25, s=100, color='red')
+        # plt.scatter(clusters[:, 0], clusters[:, 1], s=100000, alpha=0.30)
+        for i in range(k):
+            plt.scatter(df[km.labels_ == i][0], df[km.labels_ == i]
+                        [1], alpha=0.25, s=100, cmap='random_color')
 
         # print(y)
         # plt.scatter(reduced[0, :], reduced[1, :])
+        plt.show()
+
+    def cluster_number(self, df):
+        kmeans_kwargs = {"init": "random", "n_init": 10,
+                         "max_iter": 300, "random_state": 42, }
+        sse = []
+        for k in range(1, 9):
+            kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+            kmeans.fit(df)
+            sse.append(kmeans.inertia_)
+        plt.plot(range(1, 9), sse)
         plt.show()
 
 
@@ -93,10 +106,10 @@ def main():
                  "Data/girosmallveryslow2_vggfeatures.mat")
     data.EDA_VGG()
     data.PCA_VGG()
-    data.kmeans(data.VGG_df)
+    data.kmeans(data.VGG_pca, k=10)
     data.EDA_skelly()
     data.PCA_skelly()
-    data.kmeans(data.skeleton_df)
+    data.kmeans(data.skeleton_pca, k=5)
 
 
 if __name__ == '__main__':
